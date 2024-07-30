6e3632488d35be930471448961bf47d93edefb85
Julien Bayle <jbayle@gmail.com>
2024-07-30 12:34:24 +0200

    Add cancel and won animation

--- main.py
+++ main.py
@@ -1,24 +1,35 @@
 import pygame
+import copy
+import random
 from pygame import Color
-from typing import Optional, Tuple, Dict
+from typing import Optional, Tuple, Dict, Final, List
 import asyncio
 
-SQUARE_SIZE = 80
 
-GROUND_COLOR = Color(156, 96, 85)
-TRAP_COLOR = Color(226, 153, 53)
-SANCTUM_COLOR = Color(216, 72, 120)
-RIVER_COLOR = Color(72, 94, 216)
+pygame.init()
 
-WHITE = Color(255, 255, 255)
+SQUARE_SIZE: Final = 80
 
-RED_PLAYER = Color(200, 0, 0)
-YELLOW_PLAYER = Color(200, 200, 0)
-RED_PLAYER_SELECTED = Color(255, 0, 0)
-YELLOW_PLAYER_SELECTED = Color(255, 255, 0)
+GROUND_COLOR: Final = Color(156, 96, 85)
+TRAP_COLOR: Final = Color(226, 153, 53)
+SANCTUM_COLOR: Final = Color(216, 72, 120)
+RIVER_COLOR: Final = Color(72, 94, 216)
+
+WHITE: Final = Color(255, 255, 255)
+
+BUTTON_H: Final = 36
+BUTTON_W: Final = 200
+BUTTON_FONT: Final = pygame.font.Font(None, 24)
+
+RED_PLAYER: Final = Color(200, 0, 0)
+YELLOW_PLAYER: Final = Color(200, 200, 0)
+RED_PLAYER_SELECTED: Final = Color(255, 0, 0)
+YELLOW_PLAYER_SELECTED: Final = Color(255, 255, 0)
+
+FORCE_FONT: Final = pygame.font.Font(None, 36)
 
 # (x, y) : (animal force, player)
-ANIMAL_INITIAL_POSITION = {
+ANIMAL_INITIAL_POSITION: Final = {
     (0, 0): (7, 0),
     (6, 0): (6, 0),
     (1, 1): (3, 0),
@@ -41,15 +52,15 @@ ANIMAL_INITIAL_POSITION = {
 class Animal:
     force = 1
     player = 0
-    selected = False
+    _selected = False
 
     def __init__(self, config) -> None:
         self.force, self.player = config
         self.update(False)
 
     def update(self, is_selected):
-        self.selected = is_selected
-        if self.selected:
+        self._selected = is_selected
+        if self._selected:
             self.color = RED_PLAYER_SELECTED if self.player == 1 else YELLOW_PLAYER_SELECTED
         else:
             self.color = RED_PLAYER if self.player == 1 else YELLOW_PLAYER
@@ -64,7 +75,6 @@ class Square:
     def __init__(self, x, y) -> None:
         self.x = x
         self.y = y
-        self.font = pygame.font.Font(None, 36)
         self.rect = pygame.rect.Rect(self.x * SQUARE_SIZE, self.y * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE)
         if ANIMAL_INITIAL_POSITION.get((x, y)):
             self.animal = Animal(ANIMAL_INITIAL_POSITION.get((x, y)))
@@ -144,28 +154,72 @@ class Square:
         pygame.draw.rect(screen, Color(0, 200, 0) if self.highlighted else self.color(), self.rect.move(1, 1).inflate(-1, -1))
         if self.animal:
             pygame.draw.circle(screen, self.animal.color, self.rect.center, int(SQUARE_SIZE/2.3))
-            text = self.font.render(str(self.animal.force), True, WHITE)
+            text = FORCE_FONT.render(str(self.animal.force), True, WHITE)
             text_rect = text.get_rect(center=self.rect.center)
             screen.blit(text, text_rect)
 
 
-class Game:
+class Button:
+    def __init__(self, action, x, y, text) -> None:
+        self.action = action
+        self.rect = pygame.rect.Rect(x, y, BUTTON_W, BUTTON_H)
+        self.text = BUTTON_FONT.render(text, True, WHITE)
+
+    def draw(self, surface: pygame.Surface) -> None:
+        pygame.draw.rect(surface, pygame.color.Color(180, 180, 180), self.rect)
+        surface.blit(self.text, self.rect.move(10, 3))
+
+    def clic(self, pos):
+        if self.rect.collidepoint(pos):
+            self.action()
+
+
+class GameState:
     squares: Dict[Tuple[int, int], Square] = {}
     current_player = 0
-    square_selected: Optional[Square] = None
+    is_won = False
 
     def __init__(self) -> None:
-        pygame.init()
-        self.screen = pygame.display.set_mode((7 * SQUARE_SIZE, 9 * SQUARE_SIZE))
-        pygame.display.set_caption('Jungle')
-        self.clock = pygame.time.Clock()
-        self.reset()
-
-    def reset(self):
+        self.current_player = 0
         self.squares = {}
         for x in range(0, 7):
             for y in range(0, 9):
                 self.squares[(x, y)] = Square(x, y)
+
+    def how_many_animal_per_player(self) -> Tuple[int, int]:
+        animal_count_per_player: Dict[int, int] = {0: 0, 1: 0}
+        for square in self.squares.values():
+            if square.animal:
+                animal_count_per_player[square.animal.player] += 1
+        return (animal_count_per_player[0], animal_count_per_player[1])
+
+    def update(self):
+        if self.is_won:
+            for square in self.squares.values():
+                square.rect = square.rect.move(random.randint(-2, 2), random.randint(1, 5))
+
+
+class Game:
+    states: List[GameState] = []
+    square_selected: Optional[Square] = None
+    action_buttons: List[Button] = []
+
+    def __init__(self) -> None:
+        self.screen = pygame.display.set_mode((7 * SQUARE_SIZE, 9 * SQUARE_SIZE + BUTTON_H))
+        self.board_rect = self.screen.subsurface(self.screen.get_rect().clip(
+            0, BUTTON_H, self.screen.get_width(), self.screen.get_height()-BUTTON_H))
+        pygame.display.set_caption('Jungle')
+        self.clock = pygame.time.Clock()
+        self.reset()
+        self.action_buttons.append(Button(self.reset, 0, 0, "Recommencer"))
+        self.action_buttons.append(Button(self.cancel, BUTTON_W+20, 0, "Annuler"))
+
+    def reset(self):
+        self.states = [GameState()]
+
+    def cancel(self):
+        if len(self.states) > 1:
+            self.states.pop()
 
     async def run(self):
         self.draw()
@@ -177,49 +231,76 @@ class Game:
                     self.clic(event)
                 if event.type == pygame.QUIT:
                     running = False
+            self.get_current_state().update()
+            self.draw()
             self.clock.tick(60)
         pygame.quit()
 
-    def clic(self, event):
-        for _, square in self.squares.items():
-            if square.clic(event.pos):
+    def clic(self, event: pygame.event.Event):
+        for button in self.action_buttons:
+            button.clic(event.pos)
+
+        for _, square in self.get_current_state().squares.items():
+            pos_x, pos_y = event.pos
+            pos = (pos_x, pos_y - BUTTON_H)
+            if square.clic(pos):
 
                 # Unselect current animal
                 if square == self.square_selected:
                     self.unselect(square)
 
                 # Select a new animal
-                elif square.animal and self.square_selected is None and self.current_player == square.animal.player:
+                elif square.animal and self.square_selected is None and self.get_current_state().current_player == square.animal.player:
                     self.select(square)
 
                 # Move an animal
-                elif self.square_selected and self.square_selected.can_move_to(square, self.squares):
-                    square.animal = self.square_selected.animal
-                    self.square_selected.animal = None
-                    self.current_player = (self.current_player + 1) % 2
-                    self.unselect(square)
+                elif self.square_selected and self.square_selected.can_move_to(square, self.get_current_state().squares):
+                    selected = self.square_selected
+                    self.unselect(self.square_selected)
+                    new_state = copy.deepcopy(self.get_current_state())
+                    new_state_squate = new_state.squares[(square.x, square.y)]
+                    new_state_squate_selected = new_state.squares[(selected.x, selected.y)]
+                    new_state_squate.animal = new_state_squate_selected.animal
+                    new_state_squate_selected.animal = None
+                    new_state.current_player = (new_state.current_player + 1) % 2
 
-                    # Game won
-                    if square.x == 3 and square.y in [0, 8]:
-                        self.reset()
+                    # Is Game won
+                    c0, c1 = new_state.how_many_animal_per_player()
+                    if (square.x == 3 and square.y in [0, 8]) or (c0 == 0 or c1 == 0):
+                        new_state.is_won = True
+
+                    self.states.append(new_state)
 
         self.draw()
 
     def select(self, square):
         self.square_selected = square
         square.animal.update(True)
-        for _, s in self.squares.items():
-            square.highlight_if_can_move_to(s, self.squares)
+        for _, s in self.get_current_state().squares.items():
+            square.highlight_if_can_move_to(s, self.get_current_state().squares)
 
     def unselect(self, square):
         self.square_selected = None
         square.animal.update(False)
-        for _, s in self.squares.items():
+        for _, s in self.get_current_state().squares.items():
             s.highlighted = False
 
+    def get_current_state(self):
+        return self.states[len(self.states)-1]
+
     def draw(self):
-        for _, square in self.squares.items():
-            square.draw(self.screen)
+        pygame.draw.rect(self.board_rect, pygame.Color(0, 0, 0), self.board_rect.get_rect())
+        won_text = pygame.font.Font(None, 60).render("Gagné", True, WHITE)
+        self.screen.blit(won_text, self.board_rect.get_rect().move(
+            self.board_rect.get_width()/2-won_text.get_width()/2,
+            self.board_rect.get_height()/2))
+
+        for _, square in self.get_current_state().squares.items():
+            square.draw(self.board_rect)
+
+        for button in self.action_buttons:
+            button.draw(self.screen)
+
         pygame.display.update()
 
 
